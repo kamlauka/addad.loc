39e2be687cc6b2091f0798ebd1cf8d1c764ecd64
Nastya Suvorova <front6.sofona@gmail.com>
2018-08-30 12:56:32 +0300

    front add Ad 1 page

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rigger = require('gulp-rigger');
 
 gulp.task('default', function () {
     return gulp.src('./template/scss/*.scss')
@@ -9,6 +10,13 @@ gulp.task('default', function () {
         .pipe(gulp.dest('./template/css'));
 });
 
+gulp.task('html', function () {
+    gulp.src('./html/*.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('./dest'));
+})
+
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
